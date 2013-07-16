10f46d3f3937e0bbc15651830543be2ad5be77ab
LeoFR <leonardfrachet@gmail.com>
2013-07-16 14:09:46 -0400

    Premier commit

---
+++ src/ilovekitten/ILoveKitten.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ilovekitten;
+
+/**
+ *
+ * @author ljjwfr
+ */
+public class ILoveKitten {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
